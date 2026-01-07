3bbb516951c50baa38dfad73230a39d0b17f46b7
IAmAPenguin-collab <prinsli@proton.me>
2026-01-06 21:05:34 -0500

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,24 @@
+print("\033[93mWelcome To The Unofficial Flavortown Lootbox\033[0m")
+
+colors = {
+    "LEGENDARY": "\033[93m", #HOLY MOLY!!!!!
+    "PURPLE": "\033[95m",
+    "BLUE": "\033[94m",
+    "GREEN": "\033[92m",
+    "RED": "\033[0;31m",
+    "Default": "\033[0m"
+}
+
++----------+
+|##########|
+|##########|
+|####||####|
+|##########|
+|##########|
+|##########|
+|##########|
+|##########|
++----------+
+"""
+rarity = "LEGENDARY"
+print(colors[rarity] + crate + colors["RESET"])
